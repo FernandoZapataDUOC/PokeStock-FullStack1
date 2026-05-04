ebde3fb3de52a479fc3db717efce4faeeceb0884
Benjamin Gómez <be.gomezr@duocuc.cl>
2026-05-04 08:46:51 -0400

    Add files via upload
    Nigga files

---
+++ PokeStock/ms-validaciones/src/main/java/com/pokestock/ms_validaciones/MsValidacionesApplication.java
@@ -0,0 +1,13 @@
+package com.pokestock.ms_validaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsValidacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsValidacionesApplication.class, args);
+	}
+
+}
---
+++ PokeStock/ms-validaciones/src/test/java/com/pokestock/ms_validaciones/MsValidacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokestock.ms_validaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsValidacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
