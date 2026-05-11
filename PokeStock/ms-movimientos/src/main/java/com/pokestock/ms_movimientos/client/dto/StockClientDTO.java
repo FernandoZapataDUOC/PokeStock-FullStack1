e402dfaba201738fc99818e3dfde443684ca9e0d
DopeZeta <fe.zapatap@duocuc.cl>
2026-05-10 21:07:13 -0400

    BUGS corregidos
    se detectaron algunos bugs o faltas de dependencias dentro de ms-stock y validaciones que se corrigieron

--- PokeStock/ms-movimientos/src/main/java/com/pokestock/ms_movimientos/client/dto/StockClientDTO.java
+++ PokeStock/ms-movimientos/src/main/java/com/pokestock/ms_movimientos/client/dto/StockClientDTO.java
@@ -1,10 +1,23 @@
 package com.pokestock.ms_movimientos.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Data
 public class StockClientDTO {
+
     private Long id;
+
+    @JsonProperty("productoId")
     private Long productoId;
+
     private Integer cantidad;
+
+    private String lote;
+
+    private String ubicacion;
+
+    @JsonProperty("fechaActualizacion")
+    private LocalDateTime fechaActualizacion;
 }
