91097e537e8b6f2e708970f7125b8c44b84c16b1
Benjamin Gómez <be.gomezr@duocuc.cl>
2026-05-17 16:53:36 -0400

    Creado CategoriaRepository
    Se creó CategoriaRepository

---
+++ PokeStock/ms-productos/src/main/java/com/pokestock/ms_productos/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.pokestock.ms_productos.repository;
+
+import com.pokestock.ms_productos.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    Optional<Categoria> findByNombre(String nombre);
+
+    boolean existsByNombre(String nombre);
+}
